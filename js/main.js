e901ccf812de1ca915248e3dc651376864d4fa28
Daniel Monteiro Pereira <daniel_monteiro82@outlook.com>
2023-01-05 16:33:23 -0300

    JS botao criptografar e refatorar css e html

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function criptografar() {
+  var texto = document.getElementById("texto").value;
+  var displayText = document.getElementById("resultado").style.display;
+  var displayImg = document.getElementById("img-texto").style.display;
+  var boxH1 = document.getElementById("h1").style.display;
+  var boxP = document.getElementById("p").style.display;
+  var botaoCopiar = document.getElementById("btn-3").style.display;
+  document.getElementById("resultado").value = texto;
+  displayText = "table";
+  displayImg = "none";
+  boxH1 = "none";
+  boxP = "none";
+  botaoCopiar = "table";
+  document.getElementById("resultado").style.display = displayText;
+  document.getElementById("img-texto").style.display = displayImg;
+  document.getElementById("h1").style.display = boxH1;
+  document.getElementById("p").style.display = boxP;
+  document.getElementById("btn-3").style.display = botaoCopiar;
+}
+
+/*etapas 
+1 - armazenar o texto ao clicar no botao criptografar
+2 - modificar o texto conforme a criptografia definida
+3 - inserir o texto na área do resultado
+4 - modificar o display do resultado e da imagem
+5 - armazenar o texto ao clicar no botão descriptografar
+6 - repetir etapa 2 3 e 4
+7 - copiar o texto do resultado ao clicar no botão copiar
+
+*/
